54d8de012d6e85871b220b73d503422d2b34cb27
liuzonglin1 <lzlysfxx@163.com>
2023-06-04 20:21:50 +0800

    organize

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,20 +1,28 @@
 use deno_core::{
     anyhow::{Ok, Result},
+    serde::de::DeserializeOwned,
     serde_v8,
     v8::Local,
     FastString, JsRuntime, RuntimeOptions,
 };
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let options = RuntimeOptions::default();
-    let mut rt = JsRuntime::new(options);
-    let code = include_str!("basic.js");
+async fn eval<T>(rt: &mut JsRuntime, code: &'static str) -> Result<T>
+where
+    T: DeserializeOwned,
+{
     let ret = rt.execute_script("<anon>", FastString::StaticAscii(code))?;
     let result = rt.resolve_value(ret).await?;
     let scope = &mut rt.handle_scope();
     let result = Local::new(scope, result);
-    let result: String = serde_v8::from_v8(scope, result)?;
-    println!("{:#?}", result);
+    Ok(serde_v8::from_v8(scope, result)?)
+}
+
+#[tokio::test]
+async fn test() -> Result<()> {
+    let options = RuntimeOptions::default();
+    let mut rt = JsRuntime::new(options);
+    let code = include_str!("basic.js");
+    let ret: String = eval(&mut rt, code).await?;
+    println!("Rust: {:?}", ret);
     Ok(())
 }
---
+++ examples/basic_module.js
@@ -0,0 +1,9 @@
+Deno.core.print("Hello, World!");
+
+// async function hello() {
+//     // return new Promise((res, _rej) => {
+//         Deno.core.print("Hello, World! deno");
+//         // res("deno");
+//     // });
+// }
+// hello();
