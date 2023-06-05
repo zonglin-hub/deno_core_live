6a536ac2b1ef4ae4669bb065d1eb8eebbddae189
liuzonglin1 <lzlysfxx@163.com>
2023-06-05 17:05:46 +0800

    use deno_core impl js dome

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,11 +1,14 @@
 use deno_core::{
     anyhow::{Ok, Result},
+    resolve_url_or_path,
     serde::de::DeserializeOwned,
     serde_v8,
     v8::Local,
-    FastString, JsRuntime, RuntimeOptions,
+    FastString, FsModuleLoader, JsRuntime, RuntimeOptions, Snapshot,
 };
+use std::{env::current_dir, rc::Rc};
 
+#[allow(dead_code)]
 async fn eval<T>(rt: &mut JsRuntime, code: &'static str) -> Result<T>
 where
     T: DeserializeOwned,
@@ -17,6 +20,7 @@ where
     Ok(serde_v8::from_v8(scope, result)?)
 }
 
+// cargo test --package deno_core_live --example basic -- test --exact --nocapture
 #[tokio::test]
 async fn test() -> Result<()> {
     let options = RuntimeOptions::default();
@@ -24,5 +28,78 @@ async fn test() -> Result<()> {
     let code = include_str!("basic.js");
     let ret: String = eval(&mut rt, code).await?;
     println!("Rust: {:?}", ret);
+    Ok(())
+}
+
+#[tokio::test]
+async fn test1() -> Result<()> {
+    let cwd = current_dir().unwrap();
+    let options = RuntimeOptions {
+        module_loader: Some(Rc::new(FsModuleLoader)),
+        ..Default::default()
+    };
+    let mut rt = JsRuntime::new(options);
+    let expected_url = "file:///D:/.github/deno_core_live/examples/basic_modules.js";
+    let url = resolve_url_or_path(expected_url, &cwd)?;
+    let id = rt.load_main_module(&url, None).await?;
+    rt.mod_evaluate(id).await??;
+    rt.run_event_loop(false).await?;
+    Ok(())
+}
+
+#[tokio::test]
+async fn test2() -> Result<()> {
+    let cwd = current_dir().unwrap();
+    let options = RuntimeOptions {
+        module_loader: Some(Rc::new(FsModuleLoader)),
+        ..Default::default()
+    };
+    let mut rt = JsRuntime::new(options);
+    let expected_url = "file:///D:/.github/deno_core_live/examples/basic_module.js";
+    let url = resolve_url_or_path(expected_url, &cwd)?;
+    let id = rt.load_main_module(&url, None).await?;
+    let mut receiver = rt.mod_evaluate(id);
+    tokio::select! {
+        resolved = &mut receiver => {
+            resolved.expect("failed to evaluate module")?;
+        }
+        _ = rt.run_event_loop(false) => {
+            receiver.await.expect("failed to evaluate module")?;
+        }
+    }
+    Ok(())
+}
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref SNAPSHOT: &'static [u8] = {
+        let data = include_bytes!("../snapshot/main.bin");
+        let decompressed = zstd::decode_all(&data[..]).unwrap().into_boxed_slice();
+        Box::leak(decompressed)
+    };
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let cwd = current_dir().unwrap();
+    let options = RuntimeOptions {
+        module_loader: Some(Rc::new(FsModuleLoader)),
+        startup_snapshot: Some(Snapshot::Static(&*SNAPSHOT)),
+        ..Default::default()
+    };
+    let mut rt = JsRuntime::new(options);
+    let expected_url = "file:///D:/.github/deno_core_live/examples/basic_module.js";
+    let url = resolve_url_or_path(expected_url, &cwd)?;
+    let id = rt.load_main_module(&url, None).await?;
+    let mut receiver = rt.mod_evaluate(id);
+    tokio::select! {
+        resolved = &mut receiver => {
+            resolved.expect("failed to evaluate module")?;
+        }
+        _ = rt.run_event_loop(false) => {
+            receiver.await.expect("failed to evaluate module")?;
+        }
+    }
     Ok(())
 }
--- examples/basic_modules.js
+++ examples/basic_modules.js
@@ -1,9 +1,6 @@
-import {print} from "./base.js";
-
 async function hello() {
     return new Promise((res, _rej) => {
-        // Deno.core.print("Hello, World! deno");
-        print("Hello world!\n");
+        Deno.core.print("Hello, World! deno");
         res("deno");// deno 未输出
     });
 }
